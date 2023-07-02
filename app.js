5ce2880ef3b74709dde4bffd756c1177b045a5fc
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2023-07-02 20:59:24 +0200

    Update API method

--- app.js
+++ app.js
@@ -1,64 +1,60 @@
 const content = document.getElementById("catalog-content");
 const catalogButton = document.getElementById("catalog-button");
-const search = document.getElementById("search-input");
+const searchInput = document.getElementById("search-input");
+
+let cardList = [];
+
 const API_KEY = "cc4a5f02-6fa6-41fb";
 const API_ID = ["xy1-1", "swsh4-25"];
-const API_URL = `https://api.pokemontcg.io/v2/cards/${
-  API_ID[Math.floor(Math.random() * API_ID.length)]
-}`;
 
-const cardApi = async () => {
+const getRandomApiUrl = () => {
+  const randomIndex = Math.floor(Math.random() * API_ID.length);
+  const selectedId = API_ID[randomIndex];
+  return `https://api.pokemontcg.io/v2/cards/${selectedId}`;
+};
+
+const renderCard = () => {
+  const fragment = document.createDocumentFragment();
+  cardList.forEach(({ name, number, images, supertype, subtype, rarity }) => {
+    const div = document.createElement("div");
+    div.classList.add("beer");
+    div.innerHTML = `
+        <div class="card">
+          <div class="card__header">
+              <h2 class="card__text">${name}</h2>
+              <p class="card__text card__text-right">Nr.${number}</p>
+          </div>
+          <div class="description">
+              <img src="${images.small}" alt="${name}" class="card-img-top">
+              <p class="card__text"><b>Supertype: </b>${supertype}</p>
+              <p class="card__text"><b>Subtype: </b>${subtype}</p>
+              <p class="card__text"><b>Rarity: </b>${rarity}</p>
+          </div>
+        </div>
+        </div>
+        `;
+    fragment.appendChild(div);
+  });
+  content.innerHTML = "";
+  content.appendChild(fragment);
+};
+
+const getCard = async () => {
   try {
-    const response = await fetch(API_URL, {
-      method: "GET",
-      headers: {
-        "X-Api-Key": API_KEY,
-      },
-    });
-    const cardData = await response.json();
-    const cardName = cardData.data.name;
-    const cardNr = cardData.data.number;
-    const cardImage = cardData.data.images.small;
-    const cardSupertype = cardData.data.supertype;
-    const cardSubtype = cardData.data.subtypes[0];
-    const cardRarity = cardData.data.rarity;
-    return {
-      cardName,
-      cardNr,
-      cardImage,
-      cardSupertype,
-      cardSubtype,
-      cardRarity,
-    };
-  } catch (error) {
-    console.error(error);
+    const response = await fetch(getRandomApiUrl());
+    const data = await response.json();
+    cardList.push(data.data);
+    console.log(cardList);
+  } catch (err) {
+    console.log(err);
   }
 };
 
-const renderCard = async () => {
-  try {
-    const data = await cardApi();
-    const card = `
-    <div class="card">
-      <div class="card__header">
-          <h2 class="card__text">${data.cardName}</h2>
-          <p class="card__text card__text-right">Nr.${data.cardNr}</p>
-      </div>
-      <div class="description">
-          <img src="${data.cardImage}" alt="${data.cardName}" class="card-img-top">
-          <p class="card__text"><b>Supertype: </b>${data.cardSupertype}</p>
-          <p class="card__text"><b>Subtype: </b>${data.cardSubtype}</p>
-          <p class="card__text"><b>Rarity: </b>${data.cardRarity}</p>
-      </div>
-    </div>
-      `;
-    for (let i = 0; i <= 3; i++) {
-      content.innerHTML += card;
-    }
-    return card;
-  } catch (error) {
-    console.error(error);
+const addCard = async () => {
+  for (let i = 0; i <= 3; i++) {
+    await getCard();
   }
+  renderCard();
 };
 
 const searchCard = async (e) => {
@@ -79,6 +75,6 @@ const searchCard = async (e) => {
   }
 };
 
-search.addEventListener("input", searchCard);
-document.addEventListener("DOMContentLoaded", renderCard);
-catalogButton.addEventListener("click", renderCard);
+searchInput.addEventListener("input", searchCard);
+document.addEventListener("DOMContentLoaded", addCard);
+catalogButton.addEventListener("click", addCard);
